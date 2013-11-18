20e61450accf51e9a6e37ef82a65bebffcf897e3
BoppreH <boppreh@gmail.com>
2013-11-18 17:39:19 -0200

    Add missing methods

--- event.py
+++ event.py
@@ -1,84 +1,160 @@
 import random
+import math
 
 class Call(object):
-	nextId = 1
+    nextId = 1
 
-	def __init__(self, duration, tower, tower2):
-		self.id = Call.nextId
-		Call.nextId += 1
+    def __init__(self, duration, tower, tower2):
+        self.id = Call.nextId
+        Call.nextId += 1
 
-		self.duration = duration
-		self.tower = tower
-		self.tower2 = tower2
-		self.failed = False
-		self.active_tower = None
+        self.duration = duration
+        self.tower = tower
+        self.tower2 = tower2
+        self.failed = False
+        self.active_tower = None
 
-	def fail_connection(tower):
-		self.failed = True
-		self.active_tower = tower
+    def fail_connection(tower):
+        self.failed = True
+        self.active_tower = tower
 
 class CallEvent(object):
-	START_CALL = 0
-	END_CALL = 1
-	TOWER_CHANGE = 2
+    START_CALL = 0
+    END_CALL = 1
+    TOWER_CHANGE = 2
 
-	def __init__(self, call, time, type_):
-		self.call = call
-		self.time = time
-		self.type_ = type_
+    def __init__(self, call, time, type_):
+        self.call = call
+        self.time = time
+        self.type_ = type_
 
 class Tower(object):
-	all = []
+    all = []
 
-	total_duration = 0
-	min_duration = 0
-	max_duration = 0
+    total_duration = 0
+    min_duration = 0
+    max_duration = 0
 
-	total_occupation = 0
-	min_occupation = 0
-	max_occupation = 0
+    total_occupation = 0
+    min_occupation = 0
+    max_occupation = 0
 
-	def __init__(self):
-		self.neighbors = []
-		self.channels = []
-		
-		self.max_channels = 0
+    def __init__(self):
+        self.neighbors = []
+        self.channels = []
+        
+        self.max_channels = 10
 
-		self.total_calls = 0
-		self.lost_calls = 0
-		self.completed_calls = 0
+        self.total_calls = 0
+        self.lost_calls = 0
+        self.completed_calls = 0
 
-def make_events():
-	events = []
-	max_calls = 1000
-	max_duration = 500
-	min_duration = 100
-	mean_interval = 10
-	interval_range = 5
-	chance_tower_change = 0.5
+    def add_neighbors(self, *towers):
+        self.neighbors.extend(tower)
 
-	time = 0
+    def add_call(self, call):
+        if self.channels >= self.max_channels:
+            return False
 
-	for i in range(max_calls):
-		random_duration = min_duration + random.random() * (max_duration - min_duration)
-		random_selected_tower = random.choice(Tower.all)
-		random_distance = random.random() * (interval_range * 2) - interval_range + mean_interval
-		changed = random.random() < chance_tower_change
-		tower2 = None
+        channels.append(call)
+        call.active_tower = self
+        return True
 
-		if changed:
-			tower2 = random.choice(random_selected_tower.neighbors)
+    def remove_call(self, call):
+        if not call in self.channels:
+            return
 
-		call = Call(random_duration, random_selected_tower, tower2)
+        self.channels.remove(call)
+        call.active_tower = None
+        
+    def generate_events(num_calls, events):
+        time = 0
 
-		events.append(CallEvent(call, time, CallEvent.START_CALL))
-		events.append(CallEvent(call, time + random_duration, CallEvent.END_CALL))
+        for i in range(num_calls):
+            duration = self.duration_distribution() * 60
+            tower2 = None
 
-		if changed:
-			events.append(CallEvent(call, time + random_duration / 2, CallEvent.TOWER_CHANGE))
+            chance = random.random() * 100
+            if chance < self.num_drop:
+                self.tower2 = self.neighbors[1]
+            elif chance < self.num_drop + self.num_change:
+                self.tower2 = self.neighbors[0]
 
-		time += random_distance
+            call = Call(duration, self, tower2)
+            events.append(CallEvent(call, time, CallEvent.START_CALL))
+            if self.tower2:
+                events.append(CallEvent(call, time + call.duration / 2, CallEvent.TOWER_CHANGE))
+            events.append(CallEvent(call, time + call.duration, CallEvent.END_CALL))
+            time += self.interval_distribution()
 
-	events.sort(key=lambda e: e.timelist.)
+    def interval_distribution(self):
+        return -math.log(random.random()) * Tower.num_interval
 
-	return events
+    def duration_distribution(self):
+        return -math.log(random.random()) * 25
+
+class EventProcessor(object):
+    def __init__(self, events):
+        self.events = events
+        self.time = 0
+
+    def next_event(self):
+        if not self.events:
+            return
+
+        event = self.events.pop(0)
+        self.update_occupation(event.time - self.time)
+        self.process_event(event)
+
+    def update_occupation(self, timestep):
+        occupation = 0
+        for tower in self.towers:
+            occupation += len(tower.channels)
+
+        Tower.min_occupation = min(Tower.min_occupation, occupation)
+        Tower.max_occupation = min(Tower.max_occupation, occupation)
+        Tower.total_occupation += timestep * occupation
+
+    def process_event(self, event):
+        time = event.time
+        call = event.call
+
+        if event.type == CallEvent.START_CALL:
+            call.tower.total_calls += 1
+
+            if not self.transfer_call(call, call.tower):
+                return
+
+            self.update_call_duration(call)
+
+        elif event.type == CallEvent.END_CALL:
+            if call.failed:
+                return
+
+            self.update_call_duration(call)
+            call.active_tower.completed_calls += 1
+            call.active_tower.remove_call(call)
+            
+        elif event.type == CallEvent.TOWER_CHANGE:
+            if call.failed:
+                return
+
+            call.tower.remove_call(call)
+            self.transfer(call, call.tower2)
+
+    def update_call_duration(self, call):
+        Tower.total_duration += call.duration / 120
+        Tower.min_duration = min(Tower.min_duration, call.duration / 60)
+        Tower.max_duration = max(Tower.max_duration, call.duration / 60)
+
+    def transfer_call(self, call, tower):
+        if tower.add_call(call):
+            return True
+
+        tower.lost_calls += 1
+
+        call.fail_connection(tower)
+        return False
+
+if __name__ == '__main__':
+    pass
